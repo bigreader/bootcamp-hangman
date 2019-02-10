3e5de64d719eebf9b0f70191d34a9851b13e2c65
Josh <bigreaderjd1996@gmail.com>
2019-02-09 23:27:04 -0600

    built page structure, started game logic

---
+++ assets/js/game.js
@@ -0,0 +1,33 @@
+
+const words = [ "garnet", "amethyst", "pearl", "quartz", "nephrite", "lapis",
+"bismuth", "peridot", "jasper", "ruby", "sapphire", "diamond", "agate",
+"carnelian", "aquamarine", "topaz", "zircon", "rutile", "citrine", "emerald",
+"opal", "sugilite", "alexandrite", "malachite", "sardonyx", "rhodonite",
+"fluorite", "jade", "sunstone", "obsidian"];
+
+const allowedWrongGuesses = 7;
+
+
+$(document).ready(function() {
+
+	var currentWord = "";
+
+	function reset() {
+		currentWord = "";
+		$("#word").text("");
+		$("#guesses").empty();
+	}
+
+	function newGame() {
+
+	}
+
+
+
+
+
+
+
+
+
+});
