a68a0b97eb08d5404f884919bd7fce6e59a69906
Josh <bigreaderjd1996@gmail.com>
2019-02-10 23:29:25 -0600

    implemented game logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,26 +8,83 @@ const words = [ "garnet", "amethyst", "pearl", "quartz", "nephrite", "lapis",
 const allowedWrongGuesses = 7;
 
 
-$(document).ready(function() {
 
-	var currentWord = "";
+var currentWord = "";
+var guessedLetters = [];
+var repeatedGuess = "";
 
-	function reset() {
-		currentWord = "";
-		$("#word").text("");
-		$("#guesses").empty();
+function reset() {
+	currentWord = "";
+	guessedLetters = [];
+	repeatedGuess = "";
+	updateWordDisplay();
+	updateGuessesDisplay();
+}
+
+function newGame() {
+	reset();
+	currentWord = words[Math.floor(Math.random()*words.length)];
+	console.log(currentWord);
+	updateWordDisplay();
+
+	var guessedDiv = document.getElementById("guessed")
+	guessedDiv.innerHTML = '<p class="lead">Press any key to get started!</p>';
+}
+
+function updateWordDisplay() {
+	var out = [];
+	var complete = true;
+	for (var i=0; i<currentWord.length; i++) {
+		var char = currentWord.charAt(i);
+		if (guessedLetters.includes(char)) {
+			out.push(char);
+		} else {
+			out.push("_");
+			complete = false;
+		}
 	}
 
-	function newGame() {
+	var word = document.getElementById("word")
+	word.innerText = out.join(" ");
+	word.className = complete ? "text-success" : "";
+}
 
+function updateGuessesDisplay() {
+	var guessedDiv = document.getElementById("guessed")
+	guessedDiv.innerHTML = "";
+	guessedLetters.forEach(function(item) {
+		var guess = document.createElement("div");
+		guess.className = "guess";
+		if (! currentWord.includes(item)) {
+			guess.className += " wrong";
+		}
+		if (item === repeatedGuess) {
+			guess.className += " repeated";
+		}
+		guess.innerText = item;
+		guessedDiv.appendChild(guess);
+	});
+}
+
+document.onkeypress = function(event) {
+	var newGuess = event.key.toLowerCase();
+
+	if (guessedLetters.includes(newGuess)) {
+		console.log("repeated:", newGuess);
+		repeatedGuess = newGuess;
+		updateGuessesDisplay();
+	} else {
+		console.log("guess:", newGuess);
+		guessedLetters.push(event.key);
+		repeatedGuess = "";
+		updateWordDisplay();
+		updateGuessesDisplay();
 	}
 
+	// remove duplicates from guessedLetters
+	// guessedLetters = [...new Set(guessedLetters)];
+}
 
 
 
-
-
-
-
-
-});
+newGame();
